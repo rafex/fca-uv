02271ea86334864536efcb06991f4ef7760dc3eb
Raúl González <raulegleza@gmail.com>
2015-09-12 04:00:45 -0500

    hacinedo la base del mvc

---
+++ front/src/main/webapp/resources/js/login.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+    $( "#container" ).load( staticPath + "html/login.html" );
+});
+
+$('#login').click(function(event) {
+	var usuario = $('#user').val();
+    var contrasenya = $('#password').val();
+    
+    var json = { "usuario" : usuario, "contrasenya" : contrasenya };
+    alert("entre");
+    $.ajax({
+    	crossDomain: false,
+        type: "POST",
+        url: 'http://localhost:8080/front/publico/autenticacion',
+        data: JSON.stringify(json),
+        beforeSend: function(xhr) {
+        	xhr.withCredentials = true;
+            xhr.setRequestHeader("Accept", "application/json");
+            xhr.setRequestHeader("Content-Type", "application/json");
+        },
+        success: function(data){ 
+            alert(data);
+        }
+    });
+
+    event.preventDefault();
+
+});
---
+++ front/src/main/webapp/tres.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rafex
+  Date: 12/09/15
+  Time: 3:30 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+<p>Perro</p>
+</body>
+</html>
